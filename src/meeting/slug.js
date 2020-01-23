b4b298f2c52fce1e6f4594eeb6676069203b2e9d
Anton Verinov <git@zemlan.in>
2020-01-23 22:14:33 +0200

    basic layout

--- src/meeting/slug.js
+++ src/meeting/slug.js
@@ -3,6 +3,18 @@ const tmpl = require.resolve("./templates/slug.handlebars");
 
 const SLUG_REGEX = /[a-z][a-z0-9]{2,}/i;
 
+const idMapper = v => v.id;
+
+const groupByTrackId = (acc, event) => {
+  if (!acc[event.track_id]) {
+    acc[event.track_id] = [];
+  }
+
+  acc[event.track_id].push(event);
+
+  return acc;
+};
+
 module.exports = async function meetingSlug(req, res, ctx) {
   const client = await ctx.db();
   const slug = ctx.params.slug;
@@ -11,7 +23,7 @@ module.exports = async function meetingSlug(req, res, ctx) {
 
   if (slug && slug.match(SLUG_REGEX)) {
     const res = await client.query(
-      sql`SELECT id, slug FROM meeting WHERE slug = ${slug} LIMIT 1`
+      sql`SELECT id, slug, title FROM meeting WHERE slug = ${slug} LIMIT 1`
     );
 
     meeting = res.rows[0];
@@ -23,7 +35,40 @@ module.exports = async function meetingSlug(req, res, ctx) {
     return `404 Not Found`;
   }
 
+  // TODO: specify tracks order
+  const tracks = (
+    await client.query(
+      sql`SELECT id, title, color_rgb FROM track WHERE meeting_id = ${meeting.id}`
+    )
+  ).rows;
+
+  if (!tracks.length) {
+    res.statusCode = 404;
+
+    return `404 Not Found`;
+  }
+
+  const events = (
+    await client.query(
+      sql`SELECT id, track_id, title, subtitle, ts_start, ts_end
+      FROM event
+      WHERE track_id = any(${tracks.map(idMapper)})
+      ORDER BY ts_start ASC`
+    )
+  ).rows;
+
+  if (!events.length) {
+    res.statusCode = 404;
+
+    return `404 Not Found`;
+  }
+
+  const eventsByTrack = events.reduce(groupByTrackId, {});
+
   return ctx.render(tmpl, {
-    meeting
+    meeting,
+    tracks,
+    events,
+    eventsByTrack
   });
 };
