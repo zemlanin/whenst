102307e5099f0b0451f7701b2bee5a3866ad0956
Anton Verinov <git@zemlan.in>
2020-04-08 22:35:56 +0300

    send `client_id` and `client_secret` via headers

--- src/external/slack.js
+++ src/external/slack.js
@@ -26,16 +26,25 @@ module.exports = {
     };
 
     if (body && bodyContentType === APPLICATION_FORM_URLENCODED) {
+      if (body.client_id && body.client_secret) {
+        const basicAuth = Buffer.from(
+          `${body.client_id}:${body.client_secret}`
+        ).toString("base64");
+        headers["Authorization"] = `Basic ${basicAuth}`;
+        delete body.client_id;
+        delete body.client_secret;
+      }
+
       encodedBody = querystring.stringify(body);
     } else if (body && bodyContentType === APPLICATION_JSON) {
-      encodedBody = JSON.stringify(body);
-
       if (body.token) {
         // https://api.slack.com/web#posting_json
         headers["Authorization"] = `Bearer ${body.token}`;
 
         delete body.token;
       }
+
+      encodedBody = JSON.stringify(body);
     } else if (body) {
       throw new Error(
         `unknown "Content-Type" for a request body: "${bodyContentType}"`
