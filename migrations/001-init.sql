2d21fd246f80545dc985647bd69843bf02d5a780
Anton Verinov <git@zemlan.in>
2020-04-08 16:40:08 +0300

    remove no longer needed "session" table

--- migrations/001-init.sql
+++ migrations/001-init.sql
@@ -9,20 +9,6 @@ CREATE TABLE IF NOT EXISTS "slack_oauth" (
   enterprise_id text
 );
 
--- `node-connect-pg-simple/table.sql`
-CREATE TABLE IF NOT EXISTS "session" (
-  "sid" varchar NOT NULL COLLATE "default",
-  "sess" json NOT NULL,
-  "expire" timestamp(6) NOT NULL
-)
-WITH (OIDS=FALSE);
-
-ALTER TABLE "session" DROP CONSTRAINT IF EXISTS "session_pkey";
-ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
-
-CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "session" ("expire");
--- /`node-connect-pg-simple/table.sql`
-
 CREATE TABLE IF NOT EXISTS "slack_preset" (
   id bigserial PRIMARY KEY NOT NULL,
   slack_oauth_id text NOT NULL,
