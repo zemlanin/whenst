84a8302d964fbe0ce3e50bb65d07ed95d728135b
Anton Verinov <git@zemlan.in>
2020-04-11 13:15:51 +0300

    cache asset responses; enable `/cdn/` in prod

--- src/cdn.js
+++ src/cdn.js
@@ -1,41 +1,31 @@
 const fs = require("fs");
-const url = require("url");
 const path = require("path");
 
 const mime = require("mime");
 
-const config = require("./config.js");
-
 module.exports = async function CDN(req, res) {
-  if (config.cdn) {
-    res.writeHead(302, { Location: new url.URL(req.params._, config.cdn) });
-    return;
-  } else if (config.production) {
-    res.statusCode = 404;
-    return;
-  }
-
   const filepath = path.resolve(__dirname, "../static/", req.params._);
   const mimetype = mime.getType(filepath);
 
   const filestream = fs.createReadStream(filepath);
+  req.sessionID = null;
+  res.setHeader("Cache-Control", `max-age=${60 * 60 * 24}`);
+  if (mimetype) {
+    res.setHeader("Content-Type", mimetype);
+  }
 
-  return new Promise((resolve) => {
-    filestream.on("error", () => {
-      res.setHeader("Content-Type", "text/plain");
-      res.statusCode = 404;
-      resolve("404 Not Found");
+  return new Promise((resolve, reject) => {
+    filestream.on("error", (err) => {
+      if (err.code === "ENOENT") {
+        res.statusCode = 404;
+      }
+      res.removeHeader("Cache-Control");
+      reject(err);
     });
-
-    if (mimetype) {
-      res.on("pipe", () => {
-        res.setHeader("Content-Type", mimetype);
-      });
-    }
 
     filestream.pipe(res);
 
-    filestream.on("end", () => {
+    filestream.on("close", () => {
       resolve();
     });
   });
