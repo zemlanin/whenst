f73db5ea739eb8b586d5f015439463f87f091bb0
Anton Verinov <git@zemlan.in>
2020-04-17 22:57:40 +0300

    clean slack api cache on select incoming events

--- src/incoming-webhooks/slack.js
+++ src/incoming-webhooks/slack.js
@@ -6,6 +6,31 @@ const TODO_BAD_REQUEST = 400;
 
 const FIVE_MINUTES = 5 * 60 * 1000;
 
+async function eventCallback(req) {
+  const event = req.body.event;
+
+  if (event.type === "user_change") {
+    const redis = await req.redis();
+    const userId = event.user.id;
+    await redis.del(`slack:users.profile.get:${userId}`);
+    return;
+  }
+
+  if (event.type === "emoji_changed") {
+    const redis = await req.redis();
+    const teamId = req.body.team_id;
+    await redis.del(`slack:emoji.list:${teamId}`);
+    return;
+  }
+
+  if (event.type === "team_rename") {
+    const redis = await req.redis();
+    const teamId = req.body.team_id;
+    await redis.del(`slack:team.info:${teamId}`);
+    return;
+  }
+}
+
 module.exports = async function incomingWebhookSlack(req, res) {
   const slackSignature = req.headers["x-slack-signature"];
   const slackRequestTimestamp = req.headers["x-slack-request-timestamp"];
@@ -69,7 +94,7 @@ module.exports = async function incomingWebhookSlack(req, res) {
     case "url_verification":
       return { challenge: req.body.challenge };
     case "event_callback":
-      console.log(req.body.event);
+      await eventCallback(req);
       return {};
     default:
       res.statusCode = TODO_BAD_REQUEST;
