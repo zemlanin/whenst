1050f3f18b112c6b95cbd2a117c52af35267a345
Anton Verinov <git@zemlan.in>
2022-05-28 18:17:26 +0200

    relative label

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ import "urlpattern-polyfill";
 import { Temporal } from "@js-temporal/polyfill";
 
 const browserCalendar = "iso8601";
+const rtfAlways = new Intl.RelativeTimeFormat("en", {
+  numeric: "always",
+});
+const rtfAuto = new Intl.RelativeTimeFormat("en", {
+  numeric: "auto",
+});
 
 const localTZ = Temporal.Now.timeZone();
-let localDateTime = Temporal.Now.zonedDateTime(browserCalendar);
+let now = Temporal.Now.zonedDateTime(browserCalendar);
+let localDateTime = now;
+
+window.localDateTime = localDateTime;
+window.Temporal = Temporal;
 
 const [remoteTZ, timeString] = extractDataFromURL();
 
@@ -23,8 +33,10 @@ if (remoteTZ) {
       : localDateTime.toPlainTime(),
     timeZone: remoteTZ,
   });
+  localDateTime = remoteDateTime.withTimeZone(localTZ);
 
   document.getElementById("remote-time").textContent = formatDT(remoteDateTime);
+  updateRelative(true);
   document.getElementById("remote-place").textContent =
     getLocationFromTimezone(remoteTZ);
   document.getElementById("remote-url").href = new URL(
@@ -35,9 +47,91 @@ if (remoteTZ) {
   );
   document.getElementById("remote-label").hidden = false;
 
-  localDateTime = remoteDateTime.withTimeZone(localTZ);
   updateTitle(remoteDateTime, remoteTZ);
+} else {
+  updateRelative();
 }
+
+function updateRelative(withRemote) {
+  const now = Temporal.Now.zonedDateTime(browserCalendar);
+  const today = now.startOfDay();
+  const durationSinceNow = localDateTime.since(now);
+  const durationSinceToday = localDateTime.startOfDay().since(today);
+  const inTheFuture = Temporal.ZonedDateTime.compare(localDateTime, now) === 1;
+  const thisMinute = !inTheFuture && -1 * durationSinceNow.total("second") < 60;
+
+  let remoteRelative = "";
+  let localRelative = "";
+
+  if (thisMinute) {
+    remoteRelative = "is";
+    localRelative = "now";
+  } else {
+    if (inTheFuture) {
+      remoteRelative = "will be";
+    } else {
+      remoteRelative = "was";
+    }
+
+    const totalSeconds = durationSinceNow.total("second");
+    const totalMinutes = durationSinceNow.total("minute");
+    const totalHours = durationSinceNow.total("hour");
+    const totalDays = localDateTime
+      .startOfDay()
+      .since(today, { largestUnit: "day", roundingMode: "floor" })
+      .total("day");
+    const totalWeeks = localDateTime
+      .startOfDay()
+      .since(today, { largestUnit: "week", roundingMode: "floor" })
+      .total({ unit: "week", relativeTo: today });
+    const totalMonths = durationSinceToday.total({
+      unit: "month",
+      relativeTo: today,
+    });
+    const totalYears = durationSinceToday.total({
+      unit: "year",
+      relativeTo: today,
+    });
+
+    if (0 < totalSeconds && totalSeconds < 60) {
+      localRelative = rtfAuto.format(Math.ceil(totalSeconds), "second");
+    } else if (Math.ceil(Math.abs(totalMinutes)) < 120) {
+      localRelative = rtfAuto.format(Math.ceil(totalMinutes), "minute");
+    } else if (
+      Math.floor(Math.abs(totalDays)) === 0 ||
+      Math.ceil(Math.abs(totalHours)) < 12
+    ) {
+      localRelative = rtfAuto.format(Math.floor(totalHours), "hour");
+    } else if (Math.abs(totalDays) < 2) {
+      localRelative = rtfAuto.format(Math.floor(totalDays), "day");
+    } else if (Math.abs(totalDays) < 7) {
+      localRelative = `${
+        totalDays < 0 ? "past" : "upcoming"
+      } ${localDateTime.toLocaleString("en", { weekday: "long" })}`;
+    } else if (Math.abs(totalMonths) < 1) {
+      localRelative = rtfAlways.format(Math.trunc(totalWeeks), "week");
+    } else if (Math.abs(totalYears) < 1) {
+      localRelative = rtfAlways.format(Math.trunc(totalMonths), "months");
+    } else {
+      localRelative = rtfAlways.format(Math.trunc(totalYears), "year");
+    }
+  }
+
+  if (withRemote) {
+    document.getElementById("remote-relative").textContent = remoteRelative;
+  }
+
+  document.getElementById("local-relative").textContent = localRelative;
+}
+
+function scheduleUpdateRelative() {
+  setTimeout(() => {
+    updateRelative(!document.getElementById("remote-label").hidden);
+    scheduleUpdateRelative();
+  }, 1000);
+}
+
+scheduleUpdateRelative();
 
 document.getElementById("local-time").value = formatDT(localDateTime);
 
@@ -54,6 +148,7 @@ document.getElementById("local-time").addEventListener("change", (event) => {
     event.target.value
   ).toZonedDateTime(localTZ);
   updateTitle();
+  updateRelative();
 
   const localURL = getLocalURL();
   document.getElementById("local-url").href = localURL;
