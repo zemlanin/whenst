213305ff497145d8f9774022685abc74732adc33
Anton Verinov <git@zemlan.in>
2023-01-01 12:10:59 +0100

    strip `.html` from page URLs

--- posthtml.config.js
+++ posthtml.config.js
@@ -4,5 +4,16 @@ module.exports = {
     "posthtml-inline-svg": {
       cwd: ".",
     },
+    "posthtml-shorten": {
+      shortener: {
+        process: async function (url) {
+          return url.startsWith("http://") || url.startsWith("https://")
+            ? url
+            : url.replace(".html", "");
+        },
+      },
+      tag: ["a"],
+      attribute: ["href"],
+    },
   },
 };
