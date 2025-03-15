4bffff21010cd59dfd909822fb59615a693a73f1
Anton Verinov <git@zemlan.in>
2025-03-15 15:51:18 +0100

    keep the worker URL the same

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+// doing this so that worker's URL stays the same
+// - couldn't figure out how to output SW using a custom filename
+// - don't want to deal with serving both `/service-worker.js` and `/service-worker/index.js`
+import "./service-worker/index.ts";
