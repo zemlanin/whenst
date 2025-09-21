94ba75251644b54c2ddfc300088bae59dc7b4591
Anton Verinov <git@zemlan.in>
2025-09-21 21:32:45 +0200

    delete deprecated `getSessionTimezones`

--- server/db/index.ts
+++ server/db/index.ts
@@ -15,39 +15,6 @@ timezonesDB.loadExtension(
 
 export { db, timezonesDB };
 
-/** @deprecated */
-export function getSessionTimezones(sessionId: string) {
-  const account = getAccount(sessionId);
-
-  const row = account
-    ? db
-        .prepare<
-          { account_id: string },
-          { timezones: string }
-        >(`SELECT timezones FROM account_settings WHERE account_id = @account_id`)
-        .get({ account_id: account.id })
-    : db
-        .prepare<
-          { session_id: string },
-          { timezones: string }
-        >(`SELECT timezones FROM session_settings WHERE session_id = @session_id`)
-        .get({ session_id: sessionId });
-
-  if (row?.timezones) {
-    try {
-      return JSON.parse(row.timezones) as {
-        id: string;
-        label: string;
-        timezone: string;
-      }[];
-    } catch (_e) {
-      return undefined;
-    }
-  }
-
-  return undefined;
-}
-
 export function getSessionTimezonesLegacy(sessionId: string) {
   const account = getAccount(sessionId);
 
