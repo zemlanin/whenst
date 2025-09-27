cd89ec9dd3b958f286e4ce488f5949a87f130394
Anton Verinov <git@zemlan.in>
2025-09-27 12:43:34 +0200

    restructure indexeddb code
    - create it on sync if needed
    - get/put `{name, next}` objects in `sync-states` store

--- service-worker/db.ts
+++ service-worker/db.ts
@@ -1,25 +1,6 @@
 import { openDB, deleteDB, IDBPDatabase } from "idb";
 
-export const dbPromise = openDB("whenst", 1, {
-  upgrade(db, oldVersion, _newVersion, _transaction, _event) {
-    if (oldVersion < 1) {
-      const worldClockStore = db.createObjectStore("world-clock", {
-        keyPath: "id",
-      });
-      worldClockStore.createIndex("position", "position");
-      worldClockStore.createIndex("stale", "stale");
-
-      db.createObjectStore("sync-states");
-    }
-  },
-  blocked(_currentVersion, _blockedVersion, _event) {
-    console.log("blocked");
-  },
-  blocking(_currentVersion, _blockedVersion, _event) {
-    console.log("blocking");
-  },
-  terminated() {},
-});
+initDb();
 
 export async function sync() {
   if (!navigator.onLine) {
@@ -27,26 +8,55 @@ export async function sync() {
     return;
   }
 
-  const db = await dbPromise;
+  const db = await initDb();
   await pushWorldClock(db);
   await pullWorldClock(db);
 }
 
-async function getSyncState(
+function initDb() {
+  return openDB("whenst", 1, {
+    upgrade(db, oldVersion, _newVersion, _transaction, _event) {
+      if (oldVersion < 1) {
+        const worldClockStore = db.createObjectStore("world-clock", {
+          keyPath: "id",
+        });
+        worldClockStore.createIndex("position", "position");
+        worldClockStore.createIndex("stale", "stale");
+
+        db.createObjectStore("sync-states", {
+          keyPath: "name",
+        });
+      }
+    },
+    blocked(_currentVersion, _blockedVersion, _event) {
+      console.log("blocked");
+    },
+    blocking(_currentVersion, _blockedVersion, _event) {
+      console.log("blocking");
+    },
+    terminated() {},
+  });
+}
+
+async function getSyncState<T extends "world-clock">(
   db: IDBPDatabase,
-  key: "world-clock",
-): Promise<string | undefined> {
+  name: T,
+): Promise<{ name: T; next: string } | undefined> {
   const tx = db.transaction(["sync-states"], "readonly");
   const syncStatesStore = tx.objectStore("sync-states");
-  return syncStatesStore.get(key);
+  return syncStatesStore.get(name);
 }
 
 async function pullWorldClock(db: IDBPDatabase) {
   const syncState = await getSyncState(db, "world-clock");
-  let next = syncState ?? "/api/sync/world-clock";
+  let next = syncState?.next ?? "/api/sync/world-clock";
 
   do {
     const resp = await fetch(next);
+    if (!resp.ok) {
+      throw new Error(`failed to fetch ${next}`);
+    }
+
     const payload = (await resp.json()) as {
       next: string;
       changes: { id: string; tombstone: 0 | 1 }[];
@@ -66,7 +76,10 @@ async function pullWorldClock(db: IDBPDatabase) {
       }
     }
     if (next) {
-      syncStatesStore.put(next, worldClockStore.name);
+      syncStatesStore.put({
+        name: worldClockStore.name,
+        next,
+      });
     }
     await tx.done;
   } while (next);
