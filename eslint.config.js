e97a55f44174e07487908cdf9e6add04f7a0e792
Anton Verinov <git@zemlan.in>
2025-10-13 18:30:55 +0200

    bump eslint

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import { globalIgnores } from "eslint/config";
+
+export default tseslint.config(
+  { files: ["**/*.{js,mjs,cjs,ts,tsx}"] },
+  globalIgnores(["dist/", ".data/", ".parcel-cache/"]),
+  js.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    settings: {
+      react: {
+        version: "16.0",
+      },
+    },
+
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+
+      "react/no-unknown-property": [
+        "error",
+        {
+          ignore: ["onFocusIn", "onFocusOut"],
+        },
+      ],
+    },
+  },
+  {
+    files: ["icons/*.svg.tsx"],
+    rules: {
+      "react/display-name": 0,
+    },
+  },
+);
